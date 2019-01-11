c79254ad3d4d31eea04bd43880da7564b687b7e7
alan_ <alan_hernandezgarcia@hotmail.com>
2019-01-11 11:36:04 -0600

    Inicio

--- AlumnosASPMVC_Ejemplo/Startup.cs
+++ AlumnosASPMVC_Ejemplo/Startup.cs
@@ -54,9 +54,9 @@ namespace AlumnosASPMVC_Ejemplo
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute("welcome", "welcome", defaults: new { controller = "Home", action = "Index" });
+                routes.MapRoute("index", "index", defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
